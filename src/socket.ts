c3a5f4ae6b144b2e06405df9c7f4b8a43c60582a
Miguel <miguelaugl@outlook.com>
2022-02-11 15:00:18 -0300

    feat: add object to message

--- src/socket.ts
+++ src/socket.ts
@@ -6,6 +6,12 @@ enum SocketEvents {
   MESSAGE_SENT = 'message_sent'
 }
 
+type Message = {
+  user: string
+  text: string
+  date: Date
+}
+
 export function createSocket (httpServer: http.Server): void {
   const io = new Server(httpServer, {
     cors: {
@@ -16,8 +22,8 @@ export function createSocket (httpServer: http.Server): void {
   io.on('connection', (socket) => {
     console.log(`User just connected with id: ${socket.id}`)
 
-    socket.on(SocketEvents.MESSAGE_SENT, (message: string) => {
-      console.log(`User ${socket.id} sent message: ${message}`)
+    socket.on(SocketEvents.MESSAGE_SENT, (message: Message) => {
+      console.log(`User ${message.user} sent message: ${message.text}`)
       io.emit(SocketEvents.MESSAGE_SENT, message)
     })
   })
