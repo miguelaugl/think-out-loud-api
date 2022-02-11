45c9022de7bfbd86dded595ae564b6bca8c19242
Miguel <miguelaugl@outlook.com>
2022-02-11 01:06:56 -0300

    feat: add cors to socket and app

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import http from 'http'
 
+import cors from 'cors'
 import express from 'express'
 import { Server } from 'socket.io'
 
+const HTTP_SERVER_PORT = 3333
+
 const app = express()
+app.use(cors())
 const httpServer = http.createServer(app)
-const io = new Server(httpServer)
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+})
 
 io.on('connection', (connection) => {
   console.log(`User just connected with id: ${connection.id}`)
 })
 
-const HTTP_SERVER_PORT = 3333
 httpServer.listen(HTTP_SERVER_PORT, () => {
   console.log(`httpServer listening on *:${HTTP_SERVER_PORT}`)
 })
