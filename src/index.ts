a334a87778af9271b008d84153adb4c6b69d4930
Miguel <miguelaugl@outlook.com>
2022-02-10 23:08:55 -0300

    feat: server listening and socket receiving connections

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('THINK OUT LOUD API')
+import http from 'http'
+
+import express from 'express'
+import { Server } from 'socket.io'
+
+const app = express()
+const httpServer = http.createServer(app)
+const io = new Server(httpServer)
+
+io.on('connection', (connection) => {
+  console.log(`User just connected with id: ${connection.id}`)
+})
+
+const HTTP_SERVER_PORT = 3333
+httpServer.listen(HTTP_SERVER_PORT, () => {
+  console.log(`httpServer listening on *:${HTTP_SERVER_PORT}`)
+})
